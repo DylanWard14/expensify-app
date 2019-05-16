adad14c5a1f0c81c19c9076d8c03c7661d302bf0
DylanWard14 <dylan.ward@iinet.net.au>
2019-05-16 22:34:26 +1000

    Setup enzyme to allow for snapshot testing with react

---
+++ src/tests/Components/Header.test.js
@@ -0,0 +1,18 @@
+
+import { shallow } from 'enzyme';
+import React from 'react';
+import Header from '../../components/Header';
+import toJSON from 'enzyme-to-json';
+
+test('Should render header correctly', () => {
+    const wrapper = shallow(<Header/>);
+    expect(wrapper).toMatchSnapshot();
+
+    // expect(wrapper.find('h1').text()).toBe('Expensify');
+    
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header/>);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+
+    
+})
